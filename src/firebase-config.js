bdb987a261ecaf47a6c2d3a562dedc41ab898468
Pundima Lakshan <en97623@foe.sjp.ac.lk>
2023-07-21 19:56:18 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -10,22 +10,13 @@ import { getFirestore } from "@firebase/firestore";
 // Use a envfile or better way to store this data
 
 const firebaseConfig = {
-  apiKey: `AIzaSyCA_a10-QIPJKMLFcSL58cTjJZkpzxLNg0`,
-  authDomain: `evento-react-app.firebaseapp.com`,
-  projectId: `evento-react-app`,
-  storageBucket: `evento-react-app.appspot.com`,
-  messagingSenderId: `183505536296`,
-  appId: `1:183505536296:web:d5edc70c74c93551bfbc1d`,
+  apiKey: ``,
+  authDomain: ``,
+  projectId: ``,
+  storageBucket: ``,
+  messagingSenderId: ``,
+  appId: ``,
 };
-
-// const firebaseConfig = {
-//   apiKey: `${process.env.REACT_APP_API_KEY}`,
-//   authDomain: `${process.env.REACT_APP_API_KEY}`,
-//   projectId: `${process.env.REACT_APP_API_KEY}`,
-//   storageBucket: `${process.env.REACT_APP_API_KEY}`,
-//   messagingSenderId: `${process.env.REACT_APP_API_KEY}`,
-//   appId: `${process.env.REACT_APP_API_KEY}`,
-// };
 
 // Initialize Firebase
 
